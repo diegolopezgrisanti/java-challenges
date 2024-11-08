2bbd73f6aff70cd868322471472fa6bd56378a34
diegolopezgrisanti <diegolopezgrisanti@gmail.com>
2024-11-08 11:03:23 -0800

    add evenOrOdd challenge

---
+++ src/main/java/org/dalg/challenges/EvenOrOdd.java
@@ -0,0 +1,14 @@
+package org.dalg.challenges;
+
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+public class EvenOrOdd {
+
+    public static String evenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        }
+        return "Odd";
+    }
+}
