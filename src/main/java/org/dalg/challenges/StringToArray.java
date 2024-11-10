d9fde9abfa3ac54050e5de1ffc267f37477db1a5
diegolopezgrisanti <diegolopezgrisanti@gmail.com>
2024-11-10 10:20:32 -0800

    Simplified function using the split method

--- src/main/java/org/dalg/challenges/StringToArray.java
+++ src/main/java/org/dalg/challenges/StringToArray.java
@@ -11,28 +11,7 @@ Examples (Input ==> Output):
 public class StringToArray {
 
     public static String[] stringToArray(String s) {
-        int countWords = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') {
-                countWords++;
-            }
-        }
 
-        String[] solution = new String[countWords+1];
-        int positionWord = 0;
-        String word = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') {
-                solution[positionWord] = word;
-                positionWord++;
-                word = "";
-            } else {
-                word += s.charAt(i);
-            }
-        }
-        solution[positionWord] = word;
-
-        return solution;
+        return s.split(" ");
     }
 }
